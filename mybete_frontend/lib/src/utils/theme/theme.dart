14f3c87f07d68953da623260a7f327c7eaea4846
Sanjana <asanjana0823@gmail.com>
2025-01-18 16:47:01 +0530

    Add text themes to theme file

--- mybete_frontend/lib/src/utils/theme/theme.dart
+++ mybete_frontend/lib/src/utils/theme/theme.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
-    static ThemeData lightTheme = ThemeData( brightness: Brightness.light
+    static ThemeData lightTheme = ThemeData( brightness: Brightness.light,
+    textTheme: TextTheme(
+        headlineMedium: GoogleFonts.montserrat(
+            color: Colors.black87,
+            fontSize: 32,
+        ),
+        //headlineLarge: TextStyle()
+    )
     );
-    static ThemeData darkTheme = ThemeData( brightness: Brightness.dark
+    static ThemeData darkTheme = ThemeData( brightness: Brightness.dark,
+    textTheme: TextTheme(
+        headlineMedium: GoogleFonts.montserrat(
+            color: Colors.white70,
+            fontSize: 32,
+        ),
+    )
     );
 
 }
