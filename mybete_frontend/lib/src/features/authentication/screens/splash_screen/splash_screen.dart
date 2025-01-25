f4de956c36a44a11ec5fac4e75c75e043a056829
Sanjana <asanjana0823@gmail.com>
2025-01-25 10:35:04 +0530

    Add heart animation to splash screen

--- mybete_frontend/lib/src/features/authentication/screens/splash_screen/splash_screen.dart
+++ mybete_frontend/lib/src/features/authentication/screens/splash_screen/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mybete_frontend/src/constants/image_strings.dart';
 import 'package:mybete_frontend/src/constants/text.string.dart';
+import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -12,10 +13,20 @@ class SplashScreen extends StatelessWidget {
         child: Stack(
           children: [
             const Center(
-                 child: Image(image: AssetImage(splashIcon), width: 100, height: 100,)
-                ),
-                Center(child: Column(mainAxisAlignment: MainAxisAlignment.center,
-                children: [SizedBox(height: 120),Text(appName, style: Theme.of(context).textTheme.headlineSmall)],))
+                child: Image(
+              image: AssetImage(splashIcon),
+              width: 100,
+              height: 100,
+            )),
+            Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 120),
+                Text(appName, style: Theme.of(context).textTheme.headlineSmall)
+              ],
+            )),
+            Stack(children: [ Positioned(child: Lottie.asset('assets/animation/heartAnimation.json')),],),
           ],
         ),
       ),
