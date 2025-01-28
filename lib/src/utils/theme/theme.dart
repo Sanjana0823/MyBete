44b4c763794ed7bc990e5524e7563dd0359e09d5
Sanjana <asanjana0823@gmail.com>
2025-01-28 08:31:27 +0530

    Update theme

--- lib/src/utils/theme/theme.dart
+++ lib/src/utils/theme/theme.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mybete/src/utils/theme/widget_theme/text_theme.dart';
 
 class AppTheme {
-  static ThemeData lightTheme = ThemeData(brightness: Brightness.light);
-  static ThemeData darkTheme = ThemeData(brightness: Brightness.dark);
+  static ThemeData lightTheme = ThemeData(
+      brightness: Brightness.light,
+      textTheme: TextThemes.lightTheme,
+  // elevatedButtonTheme: ElevatedButtonThemeData(style: ElevatedButton.styleFrom())
+  );
+  static ThemeData darkTheme = ThemeData(brightness: Brightness.dark, textTheme: TextThemes.darkTheme);
 }
---
+++ lib/src/utils/theme/widget_theme/text_theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TextThemes{
+
+  static TextTheme lightTheme = TextTheme(
+      headlineSmall: GoogleFonts.montserrat(
+        color: Colors.black87,
+      )
+  );
+  static TextTheme darkTheme = TextTheme(
+      headlineSmall: GoogleFonts.montserrat(
+        color: Colors.white60,
+      )
+  );
+}
